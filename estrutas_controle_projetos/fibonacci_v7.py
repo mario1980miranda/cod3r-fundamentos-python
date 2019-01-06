6ae8a2c8f7424d14cc29ab1a40d5b864e7bf22c1
mario_chan15 <mario.luiz.miranda@gmail.com>
2019-01-06 13:35:23 -0200

    projeto fibonacci

---
+++ estrutas_controle_projetos/fibonacci_v7.py
@@ -0,0 +1,13 @@
+# listar os 20 primeiros numeros fibonacci
+
+
+def fibonacci(quantidade):
+    resultado = [0, 1]
+    for _ in range(2, quantidade):
+        resultado.append(sum(resultado[-2:]))
+    return resultado
+
+
+if __name__ == '__main__':
+    for fib in fibonacci(20):
+        print(fib)
---
+++ estrutas_controle_projetos/fibonacci_v8.py
@@ -0,0 +1,13 @@
+# fibonacci recursivo
+
+
+def fibonacci(quantidade, sequencia=(0, 1)):
+    # condicao de parada
+    if len(sequencia) == quantidade:
+        return sequencia
+    return fibonacci(quantidade, sequencia + (sum(sequencia[-2:]),))
+
+
+if __name__ == '__main__':
+    for fib in fibonacci(20):
+        print(fib)
