14befbb0c07e0b336283dd267865db393508093d
mario_chan15 <mario.luiz.miranda@gmail.com>
2019-01-07 22:57:53 -0200

    nocoes de comprehension

---
+++ comprehension/list_comprehension_v1.py
@@ -0,0 +1,10 @@
+# [ expressao for item in list ]
+
+dobros = [i * 2 for i in range(10)]
+print(dobros)
+
+# Versao usual
+dobros = []
+for i in range(10):
+    dobros.append(i * 2)
+print(dobros)
---
+++ comprehension/switch_3.py
@@ -0,0 +1,12 @@
+def get_tipo_dia(dia):
+    dias = {
+        (1, 7): 'Fim de Semana',
+        tuple(range(2, 7)): 'Dia de Semana',
+    }
+    dia_escolhido = (tipo for numeros, tipo in dias.items() if dia in numeros)
+    return next(dia_escolhido, '** dia_invalido **')
+
+
+if __name__ == '__main__':
+    for dia in range(1, 9):
+        print(f'{dia}: {get_tipo_dia(dia)}')
