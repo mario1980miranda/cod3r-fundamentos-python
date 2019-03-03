f5f35460adeef99186dc1960deca3ca4099cf76b
mario1980miranda <mario.luiz.miranda@gmail.com>
2019-03-02 21:44:07 -0300

    implementando map

---
+++ programacao_funcional/implementando_map_v1.py
@@ -0,0 +1,10 @@
+# Implementacao simplificada do map
+
+
+def mapear(funcao, lista):
+    for elemento in lista:
+        yield funcao(elemento)
+
+
+if __name__ == '__main__':
+    print(list(mapear(lambda x: x ** 2, [2, 3, 4])))
---
+++ programacao_funcional/implementando_map_v2.py
@@ -0,0 +1,9 @@
+# Implementacao simplificada do map
+
+
+def mapear(funcao, lista):
+    return (funcao(elemento) for elemento in lista)
+
+
+if __name__ == '__main__':
+    print(list(mapear(lambda x: x ** 2, [2, 3, 4])))
